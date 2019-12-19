8673af5e0b6ca55048eec22f2788ed19459ef2fb
Sdavis <stevendavis0622@gmail.com>
2019-12-19 13:30:36 -0700

    started homepage, import scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./homepage.component";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
